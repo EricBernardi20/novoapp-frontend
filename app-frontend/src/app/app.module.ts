b7856b6f0181eee7701f385b6974a7a3a1d619e4
samuel1333 <93929797+samuel1333@users.noreply.github.com>
2022-01-03 13:56:13 -0300

    feito a tela de login

--- app-frontend/src/app/app.module.ts
+++ app-frontend/src/app/app.module.ts
@@ -4,12 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppComponent } from './app.component';
 import { HeadComponent } from './head/head.component';
+import { HeadLoginComponent } from './login/headLogin/head-login.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HeadComponent,
-    LoginComponent
+    LoginComponent,
+    HeadLoginComponent
   ],
   imports: [
     BrowserModule,
